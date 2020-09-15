de9bba6859f7d4ff714758e1b7909a0df2e69103
kejvinrapi <kejvin.rapi@fshnstudent.info>
2020-09-15 14:47:29 +0200

    Adding solution

---
+++ Palindrome/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool istrue = false;
+            Console.WriteLine("Vendosni fjalen:");
+            string fjala = Console.ReadLine().ToString();
+            if (fjala != null)
+            {
+                foreach (var chr1 in fjala.Reverse())
+                {
+                    foreach (var chr in fjala)
+                    {
+                        if (chr.Equals(chr1))
+                        {
+                            istrue = true;
+                        }
+                        else istrue = false;
+                    }
+                }
+                if (istrue)
+                {
+                    Console.WriteLine("Fjala e vendosur eshte palindrome!");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Fjala e vendosur nuk eshte palindrome!");
+                    Console.ReadKey();
+                }
+
+            }
+        }
+    }
+}
